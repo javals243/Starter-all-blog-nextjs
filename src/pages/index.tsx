d39288aa6d139d239e7c15695a3a2daeb885d402
valere chihisa <vchihisa@gmail.com>
2023-01-23 18:58:02 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import type { InferGetStaticPropsType } from 'next';
+import { getAllPostsFrontmatter } from '@/utils/getPosts';
+import { AboutMe } from '@/components/AboutMe';
+import { Projects } from '@/components/Projects';
+import { ContactMe } from '@/components/ContactMe';
+import { LatestBlogs } from '@/components/LatestBlogs';
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+import Hero from '@/components/Hero';
+import SEO from '@/components/SEO';
+
+export default function Home({
+  latestBlogs,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <SEO />
+      <header>
+        <Navbar />
+      </header>
+      <Hero />
+      <main className="mx-auto max-w-screen-lg px-6 sm:px-8 xl:px-12">
+        <AboutMe />
+        <Projects />
+        <LatestBlogs latestBlogs={latestBlogs} />
+        <ContactMe />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allFrontMatter = await getAllPostsFrontmatter('blog');
+
+  const latestBlogs = allFrontMatter
+    .sort(({ publishedAt: a }, { publishedAt: b }) => {
+      if (a < b) {
+        return 1;
+      } else if (a > b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    .slice(0, 4);
+
+  return {
+    props: {
+      latestBlogs,
+    },
+  };
+}
